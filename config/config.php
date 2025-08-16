18f3613b478a75a8ceeb4ca2c97be3ee0a7e87d0
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-08-16 11:44:49 -0500

    update a shaday

--- config/config.php
+++ config/config.php
@@ -1,19 +1,19 @@
 <?php
 
 //Variable constante de la URL del proyecto
-const BASE_URL = "http://localhost/sis-roles";
+const BASE_URL = "http://localhost/sis-intranet-shaday";
 //Ruta de almacenamiento de archivos
 const RUTA_ARCHIVOS = "./Storage/";
 //Nombre del sistema
-const NOMBRE_SISTEMA = "Sistema de Roles";
+const NOMBRE_SISTEMA = "Sistema de intranet Shaday";
 //Nombre de la compania
-const NOMBRE_COMPANIA = "CYD TECH";
+const NOMBRE_COMPANIA = "Shaday";
 //Zona horaria
 date_default_timezone_set('America/Lima');
 
 //Datos de conexión a Base de Datos
 const DB_HOST = "localhost";
-const DB_NAME = "bd_users";
+const DB_NAME = "bd_intranet_shaday";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_CHARSET = "utf8";
@@ -32,13 +32,13 @@ const MAIL_USER = "pureba@shaday-pe.com";
 const MAIL_PASSWORD = "X5XFy46Qp?g_";
 const MAIL_ENCRYPTION = "ssl";
 const MAIL_FROM = "info@shaday-pe.com";//nombre del remitente
-const MAIL_REMITENTE = "Sistema de Roles";
+const MAIL_REMITENTE = "Sistema de intranet shaday";
 //Variables de encriptacion
 const METHOD = "AES-256-CBC";
 const SECRET_KEY = "SystemOfPredios2025";
 const SECRET_IV = "@2025BajoNaranjillo";
 //nombre de la sesion
-const SESSION_NAME = "Sistemade-gestion-de-Roles";
+const SESSION_NAME = "Sistema-de-intranet-shaday";
 //Generador de perfiles mediante nombre
 const GENERAR_PERFIL = "https://ui-avatars.com/api/?name=";
 //Variables de la API api.apis.net.pe
